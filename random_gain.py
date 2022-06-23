d03080b42c1a4f97f071978bc9ba7a71c1b8999c
cavidparker <cavidpark@gmail.com>
2022-06-23 12:49:29 +0600

    random gain audio augmentation

---
+++ random_gain.py
@@ -0,0 +1,43 @@
+import numpy as np
+import librosa
+import soundfile as sf
+import pathlib
+import tensorflow as tf
+import random
+
+
+DATAPATH = "clap"
+output_path = "output"
+data_dir = pathlib.Path(DATAPATH)
+# commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+filenames = tf.io.gfile.glob(str(data_dir) + '/*')
+# print(filenames)
+print("NUMBER OF FILE :", len(filenames))
+
+
+def random_gain(signal, min_gain_factor, max_gain_factor):
+    gain_factor = random.uniform(min_gain_factor, max_gain_factor)
+    augmented_signal = signal * gain_factor
+    return augmented_signal
+
+
+def single_image():
+    signal, sr = librosa.load("cow.wav")
+    augmented_signal = random_gain(signal, 3, 6)
+    sf.write("Augmented_random_gain.wav", augmented_signal, sr)
+    print("DONE")
+    return augmented_signal
+
+
+def save_file():
+    for y in filenames:
+        signal, sr = librosa.load(y)
+        print(y)
+        augmented_signal = random_gain(signal, 3, 6)
+        sf.write(output_path+"/"+y.split('.')
+                 [0]+"_time_augmented.wav", augmented_signal, sr)
+
+
+if __name__ == '__main__':
+    # single_image()
+    save_file()
