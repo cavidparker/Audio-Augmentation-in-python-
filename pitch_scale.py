0bdb18f797caa87a213d8299dbf347c0d84e7e9e
cavidparker <cavidpark@gmail.com>
2022-06-23 12:09:11 +0600

    pitch scale augmentation

---
+++ pitch_scale.py
@@ -0,0 +1,40 @@
+import numpy as np
+import librosa
+import soundfile as sf
+import pathlib
+import tensorflow as tf
+
+
+DATAPATH = "clap"
+output_path = "output"
+data_dir = pathlib.Path(DATAPATH)
+# commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+filenames = tf.io.gfile.glob(str(data_dir) + '/*')
+# print(filenames)
+print("NUMBER OF FILE :", len(filenames))
+
+
+def pitch_scale(signal, sr, num_semitones):
+    return librosa.effects.pitch_shift(signal, sr, num_semitones)
+
+
+def single_image():
+    signal, sr = librosa.load("cow.wav")
+    augmented_signal = pitch_scale(signal, sr, 4)
+    sf.write("Augmented_pitch_scale.wav", augmented_signal, sr)
+    print("DONE")
+    return augmented_signal
+
+
+def save_file():
+    for y in filenames:
+        signal, sr = librosa.load(y)
+        print(y)
+        augmented_signal = pitch_scale(signal, sr, 4)
+        sf.write(output_path+"/"+y.split('.')
+                 [0]+"_pitch_scale_augmented.wav", augmented_signal, sr)
+
+
+if __name__ == '__main__':
+    # single_image()
+    save_file()
